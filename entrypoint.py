95ab4d06b22b802beb49a969659c645b9d9842e2
James Strong <54325531+babylone-star@users.noreply.github.com>
2022-03-24 09:31:08 -0500

    Debugging problems with sphinx action

--- entrypoint.py
+++ entrypoint.py
@@ -9,14 +9,15 @@ from sphinx_action import action
 if __name__ == "__main__":
     print("[sphinx-action] Starting sphinx-action build.")
 
+    print("Running pre-build")
     if "INPUT_PRE-BUILD-COMMAND" in os.environ:
         pre_command = os.environ["INPUT_PRE-BUILD-COMMAND"]
         print("Running: {}".format(pre_command))
         os.system(pre_command)
 
-    github_env = action.GithubEnvironment(
-        build_command=os.environ.get("INPUT_BUILD-COMMAND"),
-    )
+    # github_env = action.GithubEnvironment(
+    #     build_command=os.environ.get("INPUT_BUILD-COMMAND"),
+    # )
 
-    # We build the doc folder passed in the inputs.
-    action.build_all_docs(github_env, [os.environ.get("INPUT_DOCS-FOLDER")])
+    # # We build the doc folder passed in the inputs.
+    # action.build_all_docs(github_env, [os.environ.get("INPUT_DOCS-FOLDER")])
